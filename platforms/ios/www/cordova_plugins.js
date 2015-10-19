6ec8cf443daa0dcc94db685c134c771bb1f83893
sathish-spindrift-git <sathish@spindriftgroup.com>
2015-10-19 21:19:04 +0100

    First commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,42 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/com.telerik.plugins.healthkit/www/HealthKit.js",
+        "id": "com.telerik.plugins.healthkit.HealthKit",
+        "clobbers": [
+            "window.plugins.healthkit"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.vibration": "0.3.13",
+    "org.apache.cordova.device": "0.3.0",
+    "com.telerik.plugins.healthkit": "0.3.7"
+}
+// BOTTOM OF METADATA
+});
---
+++ plugins/com.telerik.plugins.healthkit/src/ios/HealthKit.h
@@ -0,0 +1,33 @@
+#import <Cordova/CDV.h>
+#import <HealthKit/HealthKit.h>
+
+@interface HealthKit :CDVPlugin
+
+@property (nonatomic) HKHealthStore *healthStore;
+
+- (void) available:(CDVInvokedUrlCommand*)command;
+- (void) checkAuthStatus:(CDVInvokedUrlCommand*)command;
+- (void) requestAuthorization:(CDVInvokedUrlCommand*)command;
+
+- (void) readGender:(CDVInvokedUrlCommand*)command;
+- (void) readBloodType:(CDVInvokedUrlCommand*)command;
+- (void) readDateOfBirth:(CDVInvokedUrlCommand*)command;
+
+- (void) saveWeight:(CDVInvokedUrlCommand*)command;
+- (void) readWeight:(CDVInvokedUrlCommand*)command;
+
+- (void) saveHeight:(CDVInvokedUrlCommand*)command;
+- (void) readHeight:(CDVInvokedUrlCommand*)command;
+
+- (void) saveWorkout:(CDVInvokedUrlCommand*)command;
+- (void) findWorkouts:(CDVInvokedUrlCommand*)command;
+
+- (void) monitorSampleType:(CDVInvokedUrlCommand*)command;
+- (void) sumQuantityType:(CDVInvokedUrlCommand*)command;
+- (void) querySampleType:(CDVInvokedUrlCommand*)command;
+
+- (void) saveQuantitySample:(CDVInvokedUrlCommand*)command;
+- (void) saveCorrelation:(CDVInvokedUrlCommand*)command;
+- (void) queryCorrelationType:(CDVInvokedUrlCommand*)command;
+
+@end
